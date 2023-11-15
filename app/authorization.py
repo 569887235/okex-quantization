c8164383fa22f4e48d0205490ecaf30a10fb7218
zyzhou <569887235@qq.com>
2023-11-15 15:37:02 +0800

    update

--- app/authorization.py
+++ app/authorization.py
@@ -1,11 +1,25 @@
+'''
+Description: 
+version: 
+Author: 周子尧9028
+Date: 2023-11-15 13:31:49
+LastEditors: 周子尧9028
+LastEditTime: 2023-11-15 14:52:35
+'''
 # Get an Key and Secret
 
 api_key = ''
 api_secret = ''
 
-okex_api_key  = ''
-okex_api_secret = ''
-okex_passphrase = ''
+okex_api_key  = '9922b916-1b42-4e72-bd92-e8a65d0567f1'
+okex_api_secret = '83E83C5E953F5553826699C5D13D23EF'
+okex_passphrase = '569887235.Azx'
 recv_window = 5000
 
-dingding_token = ""
+dingding_token = "f1767d61f857df8e0d0080de724f8f2e565976964f16b374d081210954c92fad"
+
+# apikey = "9922b916-1b42-4e72-bd92-e8a65d0567f1"
+# secretkey = "83E83C5E953F5553826699C5D13D23EF"
+# IP = ""
+# 备注名 = "自动交易"
+# 权限 = "读取/交易"
